2a65e56db88ddf43bd0da72d9915c94cdfd5ad6c
Nicholas Kelley <nick@nickkelleystuff.com>
2018-06-19 16:35:44 -0400

    added persistent temp/humidity logging using EEPROM.

--- arduino-humidity-controller.ino
+++ arduino-humidity-controller.ino
@@ -6,6 +6,7 @@
 //  Thanks to ladyada and adafruit.com!
 
 #include "DHT.h"
+#include <EEPROM.h>
 
 #define DHTPIN 2      // the sensor input pin
 #define RELAYPIN 12   // the relay output pin
@@ -32,9 +33,15 @@ DHT dht(DHTPIN, DHTTYPE);
 
 // humidifier running state
 bool humidifier = true;
-// highest and lowest recorded values
-// (initialized to impossible values so it updates on the first loop)
-float high = 0, low = 100;
+
+//EEPROM addresses for high and low humidity & temps
+#define HIGHESTHUM 1
+#define LOWESTHUM 2
+#define HIGHESTTEMP 3
+#define LOWESTTEMP 4
+// check if this EEPROM address is a specific value to make sure that
+// it's (probably) set up for this sketch correctly.
+#define ISSETUP 0
 
 void setup() {
   Serial.begin(9600);
@@ -42,6 +49,15 @@ void setup() {
   pinMode(RELAYPIN, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW); // Because I'm OCD and don't want the LED on.
+
+  //check to make sure that EEPROM is setup correctly for the sketch
+  if(EEPROM.read(ISSETUP) != 42) {
+    EEPROM.write(ISSETUP, 42);
+    EEPROM.write(HIGHESTHUM, 0);
+    EEPROM.write(LOWESTHUM, 100);
+    EEPROM.write(HIGHESTTEMP, 0);
+    EEPROM.write(LOWESTTEMP, 255);
+  }
 }
 
 void loop() {
@@ -51,6 +67,7 @@ void loop() {
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
+  float t = dht.readTemperature();
 
   // Check if any reads failed and exit early (to try again).
   if (isnan(h)) {
@@ -75,18 +92,33 @@ void loop() {
   }
 
   // Check current humidity against high and low values, and update if necessary.
-  if (h > high) {
-    high = h;
+  if (h > EEPROM.read(HIGHESTHUM)) {
+    EEPROM.write(HIGHESTHUM, h);
   }
-  if (h < low) {
-    low = h;
+  if (h < EEPROM.read(LOWESTHUM)) {
+    EEPROM.write(LOWESTHUM, h);
+  }
+  
+  // Check current temp against high and low values, and update if necessary.
+  if (t > EEPROM.read(HIGHESTTEMP)) {
+    EEPROM.write(HIGHESTTEMP, h);
+  }
+  if (t < EEPROM.read(LOWESTTEMP)) {
+    EEPROM.write(LOWESTTEMP, h);
   }
   
   Serial.print("Humidity: ");
   Serial.print(h);
   Serial.print("\t\tHigh: ");
-  Serial.print(high);
+  Serial.print(EEPROM.read(HIGHESTHUM));
   Serial.print("\tLow: ");
-  Serial.print(low);
+  Serial.print(EEPROM.read(LOWESTHUM));
   Serial.print("\n");
+  Serial.print("Temp: ");
+  Serial.print(t);
+  Serial.print("\t\tHigh: ");
+  Serial.print(EEPROM.read(HIGHESTTEMP));
+  Serial.print("\tLow: ");
+  Serial.print(EEPROM.read(LOWESTTEMP));
+  Serial.print("\n\n\n");
 }
